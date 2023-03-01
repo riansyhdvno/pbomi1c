b32f9350215185c8abccbcaa7285922c5e512b2f
riansyhdvno <riansyhdvno@laptophacker1469>
2023-03-01 22:06:12 +0700

    first commit

---
+++ src/codingan_java_pertamaku/Codingan_Java_Pertamaku.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package codingan_java_pertamaku;
+
+/**
+ *
+ * @author riansyhdvno
+ */
+public class Codingan_Java_Pertamaku {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World ! ");
+        System.out.println("Riansyah Divano");
+        System.out.println("Manajemen Informatika 1.C");
+    }
+    
+}
